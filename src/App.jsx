e7b4c9b9e6d465d8a4bf9ca397ca88a9833ec871
Moiz Malik <moiz.malik1113@gmail.com>
2025-07-06 20:51:38 +0500

    bugs solved

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,7 @@ function App() {
   const [isOn, setIsOn] = useState(false);
   const [isPulled, setIsPulled] = useState(false);
 
-  const handlePull = () => {
-    setIsPulled(true);
-    setIsOn((prev) => !prev);
-
-    setTimeout(() => setIsPulled(false), 300);
-  };
-
+ 
   return (
     <div className={`scene ${isOn ? "light" : "dark"}`}>
       <div className={`bulb ${isOn ? "on" : ""}`}></div>
